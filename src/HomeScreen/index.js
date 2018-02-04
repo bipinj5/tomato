ba43ed1fd40faa01262d34294cf34e36b7db0dbd
Bipinj5 <bipin.jaiswar.bj@gmail.com>
2018-02-04 18:24:48 +0530

    rendereddata

---
+++ src/HomeScreen/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Startup from '../Login/Startup.js';
+import Signup from '../Login/Signup.js';
+import Login from '../Login/Login.js';
+import HomeScreen from './HomeScreen.js';
+import { DrawerNavigator } from 'react-navigation';
+import Search from './searchid.js';
+import SideBar from '../SideBar/SideBar.js';
+import Profile from '../ProfileScreen/index.js';
+import About from '../About/index.js';
+import Add from '../AddRestaurant/index.js';
+const HomeScreenRouter = DrawerNavigator(
+  {
+	Startup: { screen: Startup },
+	Signup: { screen: Signup },
+	Login: { screen: Login },
+	Home: { screen: HomeScreen },
+	Search: { screen: Search },
+	Profile: { screen: Profile },
+	About: { screen: About },
+	Add: { screen: Add }
+  },
+  {
+	contentComponent: props => <SideBar {...props} />
+  }
+);
+export default HomeScreenRouter;
+	
